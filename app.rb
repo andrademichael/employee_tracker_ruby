331dcbe46232608c4ec7e8263997517f277c37d8
Michael Andrade and Tracie Weitzman <michael@>
2016-12-12 14:19:07 -0800

    add update and delete methods to division class, on the front end

--- app.rb
+++ app.rb
@@ -10,3 +10,37 @@ require('pry')
 get('/') do
   erb(:index)
 end
+
+get('/divisions') do
+  @divisions = Division.all()
+  erb(:divisions)
+end
+
+post('/divisions') do
+  name = params.fetch("name")
+  division = Division.new({:name => name, :id => nil})
+  division.save()
+  erb(:division_success)
+end
+
+get('/divisions/new') do
+  erb(:division_form)
+end
+
+patch('/divisions') do
+  @division = Division.find(params.fetch("id").to_i())
+  name = params.fetch("name")
+  @division.update({:name => name})
+  erb(:division_success)
+end
+
+get('/divisions/:id') do
+  @division = Division.find(params.fetch("id").to_i)
+  erb(:division)
+end
+
+delete('/division/:id') do
+  @division = Division.find(params.fetch('id').to_i())
+  @division.delete()
+  erb(:division_success)
+end
