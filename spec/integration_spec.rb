f5826e5f24c1aa632f964acf25820a6f5a216b7c
Michael Andrade and Tracie Weitzman <michael@>
2016-12-12 16:16:01 -0800

    add second integration spec, adding new employees, passing

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,3 +1,4 @@
+require('spec_helper')
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
@@ -13,5 +14,22 @@ describe('add new division', {:type => :feature}) do
     click_link('View All Divisions')
     click_link('Human Resources')
     expect(page).to have_content('Human Resources')
+  end
+end
+
+describe('add a new employee', {:type => :feature}) do
+  it('allows user to view employees of a given division, and add new ones') do
+    test_division = Division.new({:name => "Panda Wrangling", :id => nil})
+    test_division.save()
+    visit("/")
+    click_on("View All Divisions")
+    click_on("Panda Wrangling")
+    click_on("Add an employee")
+    fill_in("employee_name", :with => "Ling-ling")
+    click_on("Add this employee")
+    visit("/")
+    click_link("View All Divisions")
+    click_link('Panda Wrangling')
+    expect(page).to have_content("Ling-ling")
   end
 end
